a191e7b6406abd01729906649be37d484ffb2e77
Akshathamk-123 <akshatha.mk.12.04@gmail.com>
2024-04-20 11:53:54 +0530

    the spring boot generated files uploaded

---
+++ spring-boot-thyme-leaf-learning-demo/src/main/java/com/example/springbootthymeleaflearningdemo/SpringBootThymeLeafLearningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleaflearningdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeLeafLearningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeLeafLearningDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-thyme-leaf-learning-demo/src/test/java/com/example/springbootthymeleaflearningdemo/SpringBootThymeLeafLearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleaflearningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeLeafLearningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
